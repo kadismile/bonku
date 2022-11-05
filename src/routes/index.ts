4d701d36f067f228ce4dc2fdff829278a9852d63
Ibrahim <ikadismile@gmail.com>
2022-11-05 21:24:01 +0000

    trying to deploy to heroku

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,4 +10,8 @@ router.get('/', function (req, res, next) {
   res.sendStatus(200);
 });
 
+router.get('/api/v1/access?tenantId=tenantId&userId=userId', function (req, res, next) {
+  res.sendStatus(200);
+});
+
 module.exports = router;
