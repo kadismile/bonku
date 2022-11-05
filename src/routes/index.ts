7bde2f4ce37745950178f70bd46429730d517174
Ibrahim <ikadismile@gmail.com>
2022-11-05 23:03:31 +0000

    updated access route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
   res.sendStatus(200);
 });
 
-router.get('/api/v1/access?tenantId=tenantId&userId=userId', function (req, res, next) {
+router.get('/api/v1/access', function (req, res, next) {
   res.sendStatus(200);
 });
 
